e5c2b1c850a625393e6cc9aa6d7d9320c483b3b3
Archit Sodhi <architk1105@gmail.com>
2018-06-06 19:40:09 +0100

    Item and item manager implemented

--- src/com/item/Item.java
+++ src/com/item/Item.java
@@ -1,66 +1,19 @@
 package com.item;
 
+import com.item.ItemManager.I_Type;
+import com.main.Main;
 import com.sabrewulf.engine.graphics.texture.Texture;
 
-import main.Main;
+public class Item {
 
-public class Item extends AbstractItem {
-
-	private int id;
-	private String i_name;
-	private Texture i_img;
-
-	protected enum I_Type {
-		item1, item2, item3, item4, item5, item6
-	}
+	private boolean activateable;
 
 	protected I_Type i_type;
 
-	public Item(Main app, int id, String i_name, Texture i_img) {
-		super(app);
-		this.id = id;
-		this.i_name = i_name;
-		this.i_img = i_img;
+	public Item(I_Type i_type) {
+
+		this.i_type = i_type;
 	}
-
-	@Override
-	public void update() {
-		switch (i_type) {
-
-		case item1:
-
-		case item2:
-
-		case item3:
-
-		case item4:
-
-		case item5:
-
-		case item6:
-		}
-
-	}
-
-	@Override
-	public void render() {
-
-		switch (i_type) {
-
-		case item1:
-
-		case item2:
-
-		case item3:
-
-		case item4:
-
-		case item5:
-
-		case item6:
-		}
-
-	};
 
 	public void action() {
 
--- src/com/main/TestState.java
+++ src/com/main/TestState.java
@@ -1,4 +1,4 @@
-package main;
+package com.main;
 
 import org.lwjgl.glfw.GLFW;
 
