d3a70f10ca643cfc03384f2c1a9b6a3ebe15ac7e
Archit Sodhi <architk1105@gmail.com>
2018-06-06 20:12:33 +0100

    kuba kubaaaa no baby no

--- src/com/gui/Inventory.java
+++ src/com/gui/Inventory.java
@@ -1,5 +1,17 @@
 package com.gui;
 
+import com.sabrewulf.engine.gui.GUI;
+
 public class Inventory {
 
+	private GUI gui;
+	private ArrayList<Label> labels;
+	public Inventory(GUI gui) {
+		this.gui = gui;
+	}
+
+	
+	
+	
+	
 }
