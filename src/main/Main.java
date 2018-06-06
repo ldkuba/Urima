cbbdc56f547156feed3468cc2b009aa0789da833
jan612 <jan612@cs.bham.ac.uk>
2018-06-06 18:14:00 +0100

    added sample code

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import com.sabrewulf.engine.application.Application;
+
+public class Main extends Application
+{
+	public Main()
+	{
+		super(800, 600, 1, "Test", false, false);
+		
+		TestState state = new TestState(this);
+		stateManager.addState(state);
+		
+		stateManager.setCurrentState(state);
+	}
+	
+	public static void main(String[] args) {
+		Main game = new Main();
+		game.run();
+	}	
+}
---
+++ src/main/TestState.java
@@ -0,0 +1,85 @@
+package main;
+
+import org.lwjgl.glfw.GLFW;
+
+import com.sabrewulf.engine.application.Application;
+import com.sabrewulf.engine.entity.Entity;
+import com.sabrewulf.engine.entity.component.SpriteComponent;
+import com.sabrewulf.engine.entity.component.TransformComponent;
+import com.sabrewulf.engine.input.InputManager;
+import com.sabrewulf.engine.maths.Vec3;
+import com.sabrewulf.engine.maths.Vec4;
+import com.sabrewulf.engine.state.AbstractState;
+
+public class TestState extends AbstractState
+{
+	private Main app;
+	private Entity testEntity;
+	
+	public TestState(Application app)
+	{
+		super(app);
+		this.app = (Main) app;
+	}
+
+	@Override
+	public void keyAction(int key, int action)
+	{	
+		if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS) {
+			app.exit();
+		}
+	}
+
+	@Override
+	public void mouseAction(int button, int action)
+	{
+		
+	}
+	
+	@Override
+	public void init() {
+		super.init();
+		
+		testEntity = new Entity("Test");
+		testEntity.addComponent(new TransformComponent());
+		testEntity.addComponent(new SpriteComponent(new Vec4(0.0f, 1.0f, 0.0f, 1.0f), 2.0f, 2.0f));
+		
+		scene.addEntity(testEntity);
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void update() {
+		super.update();
+		
+		if(app.getInputManager().isKeyPressed(GLFW.GLFW_KEY_LEFT))
+		{
+			testEntity.getTransform().move(new Vec3(0.1f, 0.0f, 0.0f));
+		}
+		
+		if(app.getInputManager().isKeyPressed(GLFW.GLFW_KEY_RIGHT))
+		{
+			testEntity.getTransform().move(new Vec3(-0.1f, 0.0f, 0.0f));
+		}
+		
+		if(app.getInputManager().isKeyPressed(GLFW.GLFW_KEY_UP))
+		{
+			testEntity.getTransform().move(new Vec3(0.0f, 0.1f, 0.0f));
+		}
+		
+		if(app.getInputManager().isKeyPressed(GLFW.GLFW_KEY_DOWN))
+		{
+			testEntity.getTransform().move(new Vec3(0.0f, -0.1f, 0.0f));
+		}
+	}
+	
+	@Override
+	public void deactivate()
+	{
+	}
+
+}
